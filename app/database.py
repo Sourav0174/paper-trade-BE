c3fb18bea7edbd3b0b7184dc725a8044cc60b090
Sourav <146104055+Sourav0174@users.noreply.github.com>
2026-02-15 19:34:05 +0530

    Refactor database.py for improved readability
    Refactor database connection setup and error handling.

--- app/database.py
+++ app/database.py
@@ -1,17 +1,44 @@
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+# from dotenv import load_dotenv
+
+# load_dotenv(dotenv_path=".env")
+
+# DATABASE_URL = os.getenv("DATABASE_URL")
+
+# print("DATABASE_URL:", DATABASE_URL)
+
+
+# if DATABASE_URL is None:
+#     raise ValueError("DATABASE_URL is not set. Check your .env file.")
+
+# engine = create_engine(DATABASE_URL)
+
+# SessionLocal = sessionmaker(
+#     autocommit=False,
+#     autoflush=False,
+#     bind=engine
+# )
+
+# Base = declarative_base()
+
+# def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 
-load_dotenv(dotenv_path=".env")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-print("DATABASE_URL:", DATABASE_URL)
-
-
-if DATABASE_URL is None:
-    raise ValueError("DATABASE_URL is not set. Check your .env file.")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 
@@ -22,6 +49,7 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
