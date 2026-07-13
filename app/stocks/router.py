0c2c3eff2b6edd98f6bf893e50d1de6fe50bdc44
Sourav <info.sourav174@gmail.com>
2026-07-13 23:05:47 +0530

    timezone

--- app/stocks/router.py
+++ app/stocks/router.py
@@ -1,10 +1,6 @@
 from fastapi import APIRouter, Query
 
-from app.stocks.schema import (
-    PaginatedStockResponse,
-    StockFilterEnum,
-    SortEnum,
-)
+from app.stocks.schema import PaginatedStockResponse
 
 from app.stocks.service import get_stocks
 
@@ -19,15 +15,11 @@ router = APIRouter(
     response_model=PaginatedStockResponse,
 )
 def fetch_stocks(
-    index: StockFilterEnum = Query(StockFilterEnum.ALL),
-    sort: SortEnum = Query(SortEnum.DEFAULT),
     page: int = Query(1, ge=1),
     limit: int = Query(20, ge=1, le=100),
     search: str | None = Query(None),
 ):
     return get_stocks(
-        index=index,
-        sort=sort,
         page=page,
         limit=limit,
         search=search,
